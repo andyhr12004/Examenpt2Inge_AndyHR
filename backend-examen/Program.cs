24c03128d68b059f18c506b462fcb6307f3651b8
andyhr12004 <andy.hernandezrojas@ucr.ac.cr>
2025-07-12 14:51:19 -0600

    feat: User Story #1: Registro de VendingMachineService en el contenedor de servicios

--- backend-examen/Program.cs
+++ backend-examen/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<VendingMachineService>();
+
 
 var app = builder.Build();
 
